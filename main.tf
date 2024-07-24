a7d7a0e7d22daded6a162f1f9fdfe529cf5864eb
paula1araujo <paula1_araujo@hotmail.com>
2024-07-24 18:13:01 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -91,8 +91,3 @@ resource "azurerm_virtual_machine_extension" "custom_script" {
     }
   SETTINGS
 }
-
-output "vm_public_ip" {
-  description = "The public IP address of the VM"
-  value       = azurerm_public_ip.public_ip.ip_address
-}

c95e5eaad1b26d99aec397ac18a9b0a4a4be6b32
paula1araujo <paula1_araujo@hotmail.com>
2024-07-24 18:29:37 -0300

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get update
 apt-get install -y docker.io
-systemctl start docker
+systemctl start -y docker
 systemctl enable docker
 docker run -d -p 80:80 --name wordpress --restart unless-stopped -e WORDPRESS_DB_HOST=localhost -e WORDPRESS_DB_USER=wordpress -e WORDPRESS_DB_PASSWORD=wordpress -e WORDPRESS_DB_NAME=wordpress wordpress
